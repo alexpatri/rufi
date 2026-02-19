846a3bd7a539f503587712f1c44fa25d18311a6c
Alexsandro <alexsandro.junior@mirante.net.br>
2026-02-19 08:45:00 -0300

    first commit

---
+++ runner/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode(1280, 720), "Titulo da Janela");
+
+  while (window.isOpen()) {
+    sf::Event event;
+
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    // window.draw();
+    window.display();
+  }
+
+  return EXIT_SUCCESS;
+}
